e7f6527bced598d8e410afba44c0b98cf48094cd
The-Anh Vu-Le <vltanh@illinois.edu>
2024-05-02 08:06:20 -0500

    add helpful functionalities and constants for coordination

---
+++ constants.py
@@ -0,0 +1,15 @@
+# File names
+
+EDGE_FILENAME = 'edge.tsv'  # List of edges
+COM_FILENAME = 'com.tsv'  # Cluster assignment
+OUTLIER_FILENAME = 'outlier.tsv'  # List of outlier nodes
+
+NODE_MAPPING_FILENAME = 'node_mapping.tsv'  # 1-to-1 mapping of node IDs
+COM_MAPPING_FILENAME = 'com_mapping.tsv'  # 1-to-1 mapping of cluster IDs
+
+DEG_DISTR_FILENAME = 'deg_distr.tsv'  # Degree distribution
+
+# Column names
+
+DEG_COLNAME = 'degree'  # Column name for degree
+COMSIZE_COLNAME = 'com_size'  # Column name for community size
