60e6b978e766dc3ff0f8c39a423c07a5bc65347e
Minhyuk Park <minpark815@gmail.com>
2024-05-02 14:31:05 -0500

    Adds distribution and sequence

--- imagine_something_like_this/compare_stats.py
+++ imagine_something_like_this/compare_stats.py
@@ -1,20 +1,29 @@
 import glob
+import json
+from pathlib import Path
 
 import click
 import networkit as nk
 import numpy as np
+import scipy
 
-def parse_stats(stats_path):
-    stat_dict = {}
+def parse_sequence(sequence_path):
+    sequence_arr = []
+    with open(sequence_path, "r") as f:
+        for line in f:
+            sequence_arr.append(float(line))
+    return sequence_arr
+
+def parse_distribution(distribution_path):
+    distribution_arr = []
+    with open(distribution_path, "r") as f:
+        for line in f:
+            distribution_arr.append(float(line))
+    return distribution_arr
+
+def parse_json(stats_path):
     with open(stats_path, "r") as f:
-        for lines in f:
-            line_arr = lines.strip().split(",")
-            stat_name = line_arr[0]
-            stat_type = line_arr[1]
-            stat_value = line_arr[2]
-            if stat_type == "scalar":
-                stat_dict[stat_name] = float(stat_value)
-    return stat_dict
+        return json.load(f)
 
 def compare_scalars(input_network_folder, input_replicates_folder, output_file):
     input_scalar_stat_dict = {
@@ -30,9 +39,9 @@ def compare_scalars(input_network_folder, input_replicates_folder, output_file):
     for current_replicate_index in range(num_replicates):
         replicates_scalar_stat_dict[current_replicate_index] = {}
 
-    input_scalar_stat_dict = parse_stats(f"{input_network_folder}/stats.dat")
+    input_scalar_stat_dict = parse_json(f"{input_network_folder}/stats.json")
     for current_replicate_index,current_replicate_folder in enumerate(replicate_folder_arr):
-        replicates_scalar_stat_dict[current_replicate_index] = parse_stats(f"{current_replicate_folder}/stats.dat")
+        replicates_scalar_stat_dict[current_replicate_index] = parse_json(f"{current_replicate_folder}/stats.json")
 
     for input_scalar_name,_ in input_scalar_stat_dict.items():
         current_value_arr = []
@@ -50,6 +59,66 @@ def compare_scalars(input_network_folder, input_replicates_folder, output_file):
         print(f"{input_scalar_name} absolute difference: {current_absolute_difference}")
 
 
+
+def compare_distributions(input_network_folder, input_replicates_folder, output_file):
+    input_distribution_stat_dict = {
+    }
+    replicates_distribution_stat_dict = {
+    }
+    replicate_folder_arr = glob.glob(f"{input_replicates_folder}/*")
+    num_replicates = len(replicate_folder_arr)
+
+    current_distribution_arr = glob.glob(f"{input_network_folder}/*.distribution")
+    distribution_name_arr = []
+    for current_distribution_file in current_distribution_arr:
+        distribution_name = Path(current_distribution_file).stem
+        distribution_name_arr.append(distribution_name)
+        input_distribution_stat_dict[distribution_name] = parse_distribution(current_distribution_file)
+
+    for current_replicate_index in range(num_replicates):
+        replicates_distribution_stat_dict[current_replicate_index] = {}
+        for current_distribution_name in distribution_name_arr:
+            current_distribution_file = f"{replicate_folder_arr[current_replicate_index]}/{current_distribution_name}.distribution"
+            replicates_distribution_stat_dict[current_replicate_index][current_distribution_name] = parse_distribution(current_distribution_file)
+
+
+    for current_distribution_name in distribution_name_arr:
+        print(f"evaluating {current_distribution_name} k-s stat")
+        for current_replicate_index in range(num_replicates):
+            input_distribution = input_distribution_stat_dict[current_distribution_name]
+            replicate_distribution = replicates_distribution_stat_dict[current_replicate_index][current_distribution_name]
+            print(f"replicate {current_replicate_index}: {scipy.stats.ks_2samp(input_distribution, replicate_distribution)}")
+
+def compare_sequences(input_network_folder, input_replicates_folder, output_file):
+    input_sequence_stat_dict = {
+    }
+    replicates_sequence_stat_dict = {
+    }
+    replicate_folder_arr = glob.glob(f"{input_replicates_folder}/*")
+    num_replicates = len(replicate_folder_arr)
+
+    current_sequence_arr = glob.glob(f"{input_network_folder}/*.sequence")
+    sequence_name_arr = []
+    for current_sequence_file in current_sequence_arr:
+        sequence_name = Path(current_sequence_file).stem
+        sequence_name_arr.append(sequence_name)
+        input_sequence_stat_dict[sequence_name] = parse_sequence(current_sequence_file)
+
+    for current_replicate_index in range(num_replicates):
+        replicates_sequence_stat_dict[current_replicate_index] = {}
+        for current_sequence_name in sequence_name_arr:
+            current_sequence_file = f"{replicate_folder_arr[current_replicate_index]}/{current_sequence_name}.sequence"
+            replicates_sequence_stat_dict[current_replicate_index][current_sequence_name] = parse_sequence(current_sequence_file)
+
+
+    for current_sequence_name in sequence_name_arr:
+        print(f"evaluating {current_sequence_name} l2 norm")
+        for current_replicate_index in range(num_replicates):
+            input_sequence = input_sequence_stat_dict[current_sequence_name]
+            replicate_sequence = replicates_sequence_stat_dict[current_replicate_index][current_sequence_name]
+            difference_sequence = np.subtract(input_sequence, replicate_sequence)
+            print(f"replicate {current_replicate_index}: {np.linalg.norm(difference_sequence, ord=2)}")
+
 @click.command()
 @click.option("--input-network-folder", required=True, type=click.Path(exists=True), help="Input network stats folder")
 @click.option("--input-replicates-folder", required=True, type=click.Path(exists=True), help="Input replicates stats folder")
@@ -58,8 +127,12 @@ def compare_stats(input_network_folder, input_replicates_folder, output_file):
     """ input network folder needs a stats.dat in there at least
     input replicates folder needs subfolders where each subfolder has a stats.dat at least
     """
-
+    print("comparing scalars")
     compare_scalars(input_network_folder, input_replicates_folder, output_file)
+    print("comparing distributions")
+    compare_distributions(input_network_folder, input_replicates_folder, output_file)
+    print("comparing sequences")
+    compare_sequences(input_network_folder, input_replicates_folder, output_file)
 
 
 if __name__ == "__main__":
--- imagine_something_like_this/compute_stats.py
+++ imagine_something_like_this/compute_stats.py
@@ -1,4 +1,7 @@
 # computing stats
+import json
+from pathlib import Path
+
 import click
 
 import networkit as nk
@@ -9,8 +12,9 @@ import numpy as np
 @click.option("--input-clustering", required=True, type=click.Path(exists=True), help="Input clustering")
 @click.option("--node-ordering", required=False, type=click.Path(exists=True), help="Node ordering")
 @click.option("--cluster-ordering", required=False, type=click.Path(exists=True), help="Cluster ordering")
-@click.option("--output-folder", required=True, type=click.Path(), help="Ouput prefix")
-def compute_stats(input_network, input_clustering, node_ordering, cluster_ordering, output_folder):
+@click.option("--output-json", required=True, type=click.Path(), help="Ouput json file")
+@click.option("--overwrite", is_flag=True, help="Whether to overwrite existing data")
+def compute_basic_stats(input_network, input_clustering, node_ordering, cluster_ordering, output_json, overwrite):
     """ input network and input clustering have no constraints
     files created inside output_folder where one file is created for generic stats and maybe more files
     for others
@@ -21,10 +25,22 @@ def compute_stats(input_network, input_clustering, node_ordering, cluster_orderi
     num_nodes = graph.numberOfNodes()
     num_edges = graph.numberOfEdges()
 
-    output_data_file = f"{output_folder}/stats.dat"
-    with open(output_data_file, "w") as f:
-        f.write(f"number of nodes,scalar,{num_nodes}\n")
-        f.write(f"number of edges,scalar,{num_edges}\n")
+    stats_dict = {}
+    stats_file = Path(output_json)
+    file_rw_bit = "w"
+    if stats_file.is_file():
+        file_rw_bit = "r"
+    with stats_file.open(file_rw_bit) as f:
+        stats_dict = json.load(f)
+
+    if "num_nodes" not in stats_dict or overwrite:
+        stats_dict["num_nodes"] = num_nodes
+    if "num_edges" not in stats_dict or overwrite:
+        stats_dict["num_edges"] = num_edges
+
+    with stats_file.open("w") as f:
+        json.dump(stats_dict, f, indent=4)
+
 
 if __name__ == "__main__":
-    compute_stats()
+    compute_basic_stats()
