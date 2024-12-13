1b3eb6a041eaff765fe689cf8c56a23a3be48229
The-Anh Vu-Le <vltanh@illinois.edu>
2024-12-13 17:53:42 -0600

    compute graph-tool stats

--- compute_gt_stats.py
+++ compute_gt_stats.py
@@ -2,11 +2,90 @@ import json
 import argparse
 from pathlib import Path
 import logging
+import time
 
-import graph_tool.all as gt
 import numpy as np
+import graph_tool.all as gt
 from scipy.sparse import linalg as la
-import matplotlib.pyplot as plt
+
+
+def compute_statistic(stat_name, compute_fn, gt_stats, overwrite=False):
+    if overwrite or stat_name not in gt_stats:
+        logging.info(f'Compute {stat_name}')
+        start_time = time.perf_counter()
+        result = compute_fn()
+        elapsed = time.perf_counter() - start_time
+        gt_stats[stat_name] = result
+        logging.info(f"{stat_name}: {elapsed}")
+    else:
+        result = gt_stats[stat_name]
+    return result
+
+
+def degree_assortativity(G):
+    deg_assort, _ = gt.scalar_assortativity(G, 'total')
+    return deg_assort
+
+
+def mean_kcore_value(G):
+    core = gt.kcore_decomposition(G)
+    return np.mean([core[v] for v in G.vertices()])
+
+
+def local_clustering_coefficient(G):
+    local_ccoeffs = gt.local_clustering(G)
+    return np.mean([local_ccoeffs[v] for v in G.vertices()])
+
+
+def global_clustering_coefficient(G):
+    global_ccoeff, _ = gt.global_clustering(G)
+    return global_ccoeff
+
+
+def leading_eigenvalue_adjacency(G):
+    largest_eigval_A, _ = gt.eigenvector(G)
+    return largest_eigval_A
+
+
+def leading_eigenvalue_hashimoto(G):
+    H_mtx = gt.hashimoto(G)
+    eigvals_H = la.eigs(H_mtx, k=1, return_eigenvectors=False, which='LR')
+    return eigvals_H[0].real
+
+
+def characteristic_time_random_walk(G):
+    largest_cc = gt.extract_largest_component(G)
+    T = gt.transition(largest_cc)
+    eigvals_T = la.eigs(T, k=2, return_eigenvectors=False, which='LR')
+    return -np.log(np.abs(eigvals_T[0].real))
+
+
+def pseudo_diameter(G):
+    pseudo_diameter, *_ = gt.pseudo_diameter(G)
+    return pseudo_diameter
+
+
+def node_percolation_targeted(G):
+    vertices = sorted([v for v in G.vertices()], key=lambda v: v.out_degree())
+    sizes, _ = gt.vertex_percolation(G, vertices)
+    return np.mean(sizes) / G.num_vertices()
+
+
+def node_percolation_random(G):
+    n_trials = 5
+    Rr = 0.0
+    vertices = [v for v in G.vertices()]
+    for i in range(n_trials):
+        np.random.shuffle(vertices)
+        sizes2, _ = gt.vertex_percolation(G, vertices)
+        Rr += np.mean(sizes2) / G.num_vertices() / n_trials
+    return Rr
+
+
+def fraction_giant_component(G):
+    giant_cc = gt.extract_largest_component(G)
+    return giant_cc.num_vertices() / G.num_vertices()
+
 
 parser = argparse.ArgumentParser(
     description='Compute excess edges in a graph.'
@@ -16,124 +95,91 @@ parser.add_argument(
     type=str,
     help='Path to the network file',
 )
-# parser.add_argument(
-#     '--output',
-#     type=str,
-#     help='Path to the output JSON file',
-# )
+parser.add_argument(
+    '--output',
+    type=str,
+    help='Path to the output directory',
+)
+parser.add_argument(
+    '--overwrite',
+    action='store_true',
+    help='Overwrite existing statistics',
+)
 
 args = parser.parse_args()
-
 network_fp = Path(args.input)
-# output_fp = Path(args.output)
+output_dir = Path(args.output)
+is_overwrite = args.overwrite
 
 assert network_fp.exists(), f'File not found: {network_fp}'
 assert network_fp.is_file(), f'Not a file: {network_fp}'
-# assert output_fp.parent.exists(), f'Directory not found: {output_fp.parent}'
-# assert output_fp.parent.is_dir(), f'Not a directory: {output_fp.parent}'
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
+output_dir.mkdir(parents=True, exist_ok=True)
+log_path = output_dir / 'gt_stats.log'
+logging.basicConfig(
+    filename=log_path,
+    filemode='w' if is_overwrite else 'a',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
 
-logger.info(f'Input: {network_fp}')
-# logger.info(f'Output: {output_fp}')
+logging.info(f'Input: {network_fp}')
+logging.info(f'Output: {output_dir}')
 
+# If gt_stats.json exists, load it
+gt_stats_fp = output_dir / 'gt_stats.json'
+if gt_stats_fp.exists():
+    with open(gt_stats_fp, 'r') as f:
+        gt_stats = json.load(f)
+else:
+    gt_stats = dict()
+
+statistics_functions = {
+    'deg_assort': degree_assortativity,
+    'mean_kcore': mean_kcore_value,
+    'local_ccoeff': local_clustering_coefficient,
+    'global_ccoeff': global_clustering_coefficient,
+    # 'l_eigval_A': leading_eigenvalue_adjacency,
+    # 'l_eigval_H': leading_eigenvalue_hashimoto,
+    # 'tau': characteristic_time_random_walk,
+    'pseudo_diameter': pseudo_diameter,
+    'node_percolation_targeted': node_percolation_targeted,
+    'node_percolation_random': node_percolation_random,
+    'frac_giant_ccomp': fraction_giant_component,
+}
+
+# Check if all statistics are already computed
+if not is_overwrite:
+    missing_stats = [
+        stat for stat in statistics_functions if stat not in gt_stats]
+    if not missing_stats:
+        logging.info('All statistics are already computed.')
+        exit(0)
+
+# Load the graph
 G = gt.load_graph_from_csv(
     str(network_fp),
     directed=False,
     csv_options={'delimiter': '\t'},
 )
-
-# n_edges = G.num_edges()
-
 gt.remove_parallel_edges(G)
-# n_parallel_edges = n_edges - G.num_edges()
-
 gt.remove_self_loops(G)
-# n_self_loops = n_edges - n_parallel_edges - G.num_edges()
 
-# logger.info(f'n_edges: {n_edges}')
-# logger.info(f'n_parallel_edges: {n_parallel_edges}')
-# logger.info(f'n_self_loops: {n_self_loops}')
-# logger.info(f'ratio_parallel_edges: {n_parallel_edges / n_edges}')
-# logger.info(f'ratio_self_loops: {n_self_loops / n_edges}')
+# Compute the statistics
+for stat_name, compute_fn in statistics_functions.items():
+    gt_stats[stat_name] = \
+        compute_statistic(
+            stat_name,
+            lambda fn=compute_fn: fn(G),
+            gt_stats,
+            overwrite=is_overwrite,
+    )
 
-# with open(output_fp, 'w') as f:
-#     json.dump({
-#         'n_edges': n_edges,
-#         'n_parallel_edges': n_parallel_edges,
-#         'n_self_loops': n_self_loops,
-#         'ratio_parallel_edges': n_parallel_edges / n_edges,
-#         'ratio_self_loops': n_self_loops / n_edges,
-#     }, f, indent=4)
-
-# Degree assortativity
-# assort, _ = gt.assortativity(G, 'total')
-deg_assort, _ = gt.scalar_assortativity(G, 'total')
-logger.info(f'Scalar assortativity: {deg_assort}')
-
-# Mean k-core value
-core = gt.kcore_decomposition(G)
-mean_k_core = np.mean([
-    core[v] for v in G.vertices()
-])
-logger.info(f'Mean k-core value: {mean_k_core}')
-
-# Local clustering coefficient
-local_ccoeffs = gt.local_clustering(G)
-local_ccoeff = np.mean([
-    local_ccoeffs[v] for v in G.vertices()
-])
-logger.info(f'Mean local clustering coefficient: {local_ccoeff}')
-
-# Global clustering coefficient
-global_ccoeff, _ = gt.global_clustering(G)
-logger.info(f'Global clustering coefficient: {global_ccoeff}')
-
-# Leading eigenvalue of the adjacency matrix
-largest_eigval_A, _ = gt.eigenvector(G)
-logger.info(f'Leading eigenvalue of the adjacency matrix: {largest_eigval_A}')
-
-# Leading eigenvalue of the Hashimoto matrix
-H_mtx = gt.hashimoto(G)
-eigvals_H = la.eigs(H_mtx, k=1, return_eigenvectors=False, which='LR')
-largest_eigval_H = eigvals_H[0].real
-logger.info(f'Leading eigenvalue of the Hashimoto matrix: {largest_eigval_H}')
-
-# Characteristic time of a random walk
-# Get the largest connected component
-largest_cc = gt.extract_largest_component(G)
-# Compute the transition matrix
-T = gt.transition(largest_cc)
-# Compute the 2nd largest eigenvalues of the transition matrix
-eigvals_T = la.eigs(T, k=2, return_eigenvectors=False, which='LR')
-# Compute the characteristic time
-tau = -np.log(np.abs(eigvals_T[0].real))
-logger.info(f'Characteristic time of a random walk: {tau}')
-
-# Psuedo-diameter
-pseudo_diameter, *_ = gt.pseudo_diameter(G)
-logger.info(f'Pseudo-diameter: {pseudo_diameter}')
-
-# Node percolation profile (targeted removal)
-vertices = sorted([v for v in G.vertices()], key=lambda v: v.out_degree())
-sizes, _ = gt.vertex_percolation(G, vertices)
-Rt = np.mean(sizes) / G.num_vertices()
-logger.info(f'Node percolation profile (targeted removal): {Rt}')
-
-# Node percolation profile (random removal)
-n_trials = 100
-Rr = 0.0
-vertices = [v for v in G.vertices()]
-for i in range(n_trials):
-    # node_orderings = np.random.permutation(n_nodes)
-    np.random.shuffle(vertices)
-    sizes2, _ = gt.vertex_percolation(G, vertices)
-    Rr += np.mean(sizes2) / G.num_vertices() / n_trials
-logger.info(f'Node percolation profile (random removal): {Rr}')
-
-# Fraction of vertices in the giant component
-giant_cc = gt.extract_largest_component(G)
-giant_frac = giant_cc.num_vertices() / G.num_vertices()
-logger.info(f'Fraction of vertices in the giant component: {giant_frac}')
+    # Save the computed statistics
+    with open(gt_stats_fp, 'w') as f:
+        json.dump(gt_stats, f, indent=4)
